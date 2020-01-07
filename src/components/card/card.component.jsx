b206b4566c4f5941e0a28d5181986794b54bf05e
Jozsa Tibold <jozsatibold@gmail.com>
2020-01-07 22:21:51 +0200

    adding files for github pages

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './card.style.css';
+
+export const Card = (props) => {
+  const {monster} = props;
+  return <div className='card-container'>
+    <img alt="monster" src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}/>
+    <h1>{monster.name}</h1>
+    <p>{monster.email}</p>
+  </div>;
+};
